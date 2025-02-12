96ba3cad2ea81ed025f0b32a710c4dc3b8c64b0d
Paritosh02 <104285478+Paritosh02@users.noreply.github.com>
2025-02-12 16:45:14 +0530

    Add files via upload

---
+++ LAB_4.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.DataFrame({
+    'ord_no': [np.nan, np.nan, 70002, np.nan, np.nan, 70005, np.nan, 70010, 70003, 70012, np.nan, np.nan],
+    'purch_amt': [np.nan, 270.65, 65.26, np.nan, 948.5, 2400.6, 5760, 1983.43, 2480.4, 250.45, 75.29, np.nan],
+    'ord_date': [np.nan, '2012-09-10', np.nan, np.nan, '2012-09-10', '2012-07-27', '2012-09-10', '2012-10-10',
+                 '2012-10-10', '2012-06-27', '2012-08-17', np.nan],
+    'customer_id': [np.nan, 3001, 3001, np.nan, 3002, 3001, 3001, 3004, 3003, 3002, 3001, np.nan]
+})
+print("Original Value of dataframe")
+print(df1)
+df1_cleaned = df1.dropna(subset=['ord_no', 'purch_amt'])
+print("\nDataFrame after Dropping Rows Where Specific Columns Have Missing Values:")
+print(df1_cleaned)
